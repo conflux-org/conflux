3d41b6105df5e2cc49422e0ee57f892981b8ecd7
huntertoby <tobby8668@gmail.com>
2026-07-28 23:20:08 +0800

    style(backend-db): resolve ruff import sort and combined with statement lint errors

--- backend/test/test_aiven.py
+++ backend/test/test_aiven.py
@@ -1,4 +1,5 @@
 import os
+
 import psycopg
 import pytest
 
@@ -10,13 +11,12 @@ import pytest
 def test_aiven_connection():
     service_uri = os.environ["aiven_TOKEN"]
     try:
-        with psycopg.connect(service_uri) as conn:
-            with conn.cursor() as cur:
-                cur.execute("SELECT version();")
-                db_version = cur.fetchone()
-                assert db_version is not None
-                print("成功連線至 Aiven PostgreSQL！")
-                print("資料庫版本：", db_version[0])
+        with psycopg.connect(service_uri) as conn, conn.cursor() as cur:
+            cur.execute("SELECT version();")
+            db_version = cur.fetchone()
+            assert db_version is not None
+            print("成功連線至 Aiven PostgreSQL！")
+            print("資料庫版本：", db_version[0])
 
     except Exception as error:  # noqa: BLE001
         pytest.fail(f"連線失敗：{error}")
