8dfc1256a3230170e2477bcdf9e12f53657ae81b
huntertoby <tobby8668@gmail.com>
2026-08-04 15:11:21 +0800

    refactor(backend-api): update URL patterns to single resource and plural collection format

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,13 @@ from api import auth, channel, guild, user
 urlpatterns = [
     path("auth/login/", auth.login, name="login"),
     path("auth/signup/", auth.sign_up, name="signup"),
-    path("user/<int:user_id>/guild/", user.get_guilds_by_user_id, name="user-guilds"),
     path(
-        "guild/<int:guild_id>/channel/",
+        "user/<int:user_id>/guilds/",
+        user.get_guilds_by_user_id,
+        name="user-guilds",
+    ),
+    path(
+        "guild/<int:guild_id>/channels/",
         guild.get_channels_by_guild_id,
         name="guild-channels",
     ),
